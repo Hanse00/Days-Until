bd3b9437eb1a5ec51d925c4bfd4eb109e9522bfd
Philip Hansen <me@philiphansen.dk>
2015-11-08 17:35:51 +0100

    Playing around with argparse
    Using argparse to provide the end date as a positional argument, rather
    then entering it into the program during runtime. This is not actually
    implemented in running the program yet.

--- days-until.py
+++ days-until.py
@@ -1,15 +1,34 @@
+import argparse
 import datetime
 
-a = datetime.date.today()
-entry = raw_input("Enter date in format YYYY-MM-DD: ")
+def iso_date(string):
+    try:
+        date = datetime.datetime.strptime(string, "%Y-%m-%d").date()
+        return date
+    except ValueError:
+        msg = "'{}' is not an ISO formatted date".format(string)
+        raise argparse.ArgumentTypeError(msg)
 
-b = datetime.datetime.strptime(entry, "%Y-%m-%d").date()
 
-d = b - a
+program_desc = ("A tool for calculating the number of days (Starting today) " +
+    "until a given date.")
+date_arg_desc = ("The destination date, formatted as a zero padded ISO date" +
+    "string. Eg. 2015-11-08")
 
-if d.days <= 0:
-    print "b must be later than a!"
-elif d.days == 1:
-    print "1 day"
-else:
-    print "{} days".format(d.days)
+parser = argparse.ArgumentParser(description=program_desc)
+parser.add_argument("date", type=iso_date, help=date_arg_desc)
+print parser.parse_args("2015-12-24 -h".split())
+
+#a = datetime.date.today()
+#entry = raw_input("Enter date in format YYYY-MM-DD: ")
+
+#b = datetime.datetime.strptime(entry, "%Y-%m-%d").date()
+
+#d = b - a
+
+#if d.days <= 0:
+#    print "b must be later than a!"
+#elif d.days == 1:
+#    print "1 day"
+#else:
+#    print "{} days".format(d.days)
