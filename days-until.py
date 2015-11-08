08b1cf61c8b4539a6b0a57eed48e1c080b1392d3
Philip Hansen <me@philiphansen.dk>
2015-11-08 18:27:06 +0100

    Implemented argparse
    The argument parsing in now implemented in the base program

--- days-until.py
+++ days-until.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime
+import sys
 
 def iso_date(string):
     try:
@@ -9,26 +10,26 @@ def iso_date(string):
         msg = "'{}' is not an ISO formatted date".format(string)
         raise argparse.ArgumentTypeError(msg)
 
-
 program_desc = ("A tool for calculating the number of days (Starting today) " +
     "until a given date.")
 date_arg_desc = ("The destination date, formatted as a zero padded ISO date" +
     "string. Eg. 2015-11-08")
 
-parser = argparse.ArgumentParser(description=program_desc)
-parser.add_argument("date", type=iso_date, help=date_arg_desc)
-print parser.parse_args("2015-12-24 -h".split())
+def main(args):
+    a = datetime.date.today()
 
-#a = datetime.date.today()
-#entry = raw_input("Enter date in format YYYY-MM-DD: ")
+    parser = argparse.ArgumentParser(description=program_desc)
+    parser.add_argument("date", type=iso_date, help=date_arg_desc)
+    b = vars(parser.parse_args(args))["date"]
 
-#b = datetime.datetime.strptime(entry, "%Y-%m-%d").date()
+    delta = b - a
 
-#d = b - a
+    if delta.days <= 0:
+        print "b must be later than a!"
+    elif delta.days == 1:
+        print "1 day"
+    else:
+        print "{} days".format(delta.days)
 
-#if d.days <= 0:
-#    print "b must be later than a!"
-#elif d.days == 1:
-#    print "1 day"
-#else:
-#    print "{} days".format(d.days)
+if __name__ == "__main__":
+    main(sys.argv[1:])
