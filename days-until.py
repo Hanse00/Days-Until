b914ea427b91d4b8176c31a438a998dcf99dae4b
Philip Hansen <me@philiphansen.dk>
2015-11-08 16:47:00 +0100

    Playing around with date comparison

--- days-until.py
+++ days-until.py
@@ -1,1 +1,15 @@
+import datetime
+
 print "Hello World!"
+
+a = datetime.date(2015, 11, 8)
+b = datetime.date(2015, 11, 9)
+
+d = b - a
+
+if d.days <= 0:
+    print "b must be later than a!"
+elif d.days == 1:
+    print "1 day"
+else:
+    print "{} days".format(d.days)
