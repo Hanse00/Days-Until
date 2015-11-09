2ac0768f68f0a7fc4c32b0351b98a7c4a9c615ee
Philip Hansen <me@philiphansen.dk>
2015-11-09 11:54:27 +0100

    Code refactoring
    Started giving better function and variable names, making the code more
    readable.

--- days-until.py
+++ days-until.py
@@ -10,19 +10,26 @@ def iso_date(string):
         msg = "'{}' is not an ISO formatted date".format(string)
         raise argparse.ArgumentTypeError(msg)
 
-program_desc = ("A tool for calculating the number of days (Starting today) " +
-    "until a given date.")
-date_arg_desc = ("The destination date, formatted as a zero padded ISO date" +
-    "string. Eg. 2015-11-08")
+def _parse_arguments(args):
+    program_desc = ("a tool for calculating the number of days " +
+        "(starting today) until a given date")
+    date_arg_desc = ("the destination date, formatted as a zero padded ISO " +
+        "date string, Eg. 2015-11-08")
+    human_arg_desc = ("human readable format, appends the word day or days to" +
+        "the output as appropriate")
+    help_arg_desc = "show this help message and exit"
+
+    parser = argparse.ArgumentParser(description=program_desc, add_help=False)
+    parser.add_argument("date", type=iso_date, help=date_arg_desc)
+    parser.add_argument("-h", action="store_true", help=human_arg_desc)
+    parser.add_argument("--help", action="help", help=help_arg_desc)
+    return vars(parser.parse_args(args))
 
 def main(args):
-    a = datetime.date.today()
+    start = datetime.date.today()
+    parsed_args = _parse_arguments(args)
 
-    parser = argparse.ArgumentParser(description=program_desc)
-    parser.add_argument("date", type=iso_date, help=date_arg_desc)
-    b = vars(parser.parse_args(args))["date"]
-
-    delta = b - a
+    delta = parsed_args["date"] - start
 
     if delta.days <= 0:
         print "b must be later than a!"
